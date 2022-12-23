5b9cfdd10dd8e4d9d8c4223ee8a37ac5f2982866
dkdkdo123 <dkdkdo123@gmail.com>
2022-12-23 21:12:58 +0900

    12/27 수정2

--- app/src/main/java/com/example/myapplication/result.java
+++ app/src/main/java/com/example/myapplication/result.java
@@ -6,11 +6,10 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class result extends AppCompatActivity {
-    Button btn_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        btn_result = (Button) findViewById(R.id.button_result);
+
     }
 }
